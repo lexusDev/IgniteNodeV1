5eab07cefdc1604a612ddd6a49c1cc2ec441f527
Alex Monteiro <alex.monteiro@digiage.com.br>
2021-04-26 10:42:53 -0300

    Adding errors implementation

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -27,11 +28,11 @@ class AuthenticateUserUseCase {
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
 
-    if (!user) throw new Error("Email or password incorrect!");
+    if (!user) throw new AppError("Email or password incorrect!");
 
     const passwordMatch = await compare(password, user.password);
 
-    if (!passwordMatch) throw new Error("Email or password incorrect!");
+    if (!passwordMatch) throw new AppError("Email or password incorrect!");
 
     const token = sign({}, "4c2ade7acd1cecdfc6d10e01ec7af672", {
       subject: user.id,
