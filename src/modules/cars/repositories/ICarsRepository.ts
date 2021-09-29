eb17507ab6d51a1e8823bf319564556ceafc5a57
Alex Monteiro <alex.monteiro@digiage.com.br>
2021-09-29 10:08:29 -0300

    TDD casas

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,9 @@
+import { ICreateCarDTO } from "../dtos/ICreateCarDTO";
+import { Car } from "../infra/typeorm/entities/Car";
+
+interface ICarsRepository {
+  findByLicense(license_plate: string): Promise<Car>;
+  create(data: ICreateCarDTO): Promise<Car>;
+}
+
+export { ICarsRepository };
